63449f5ee4e41ffc5ba96370feac3af652638779
zadmario <margruszk@gmail.com>
2022-01-15 10:29:55 +0100

    try to fix ustvgo.tv

--- IPTVPlayer/version.py
+++ IPTVPlayer/version.py
@@ -1,7 +1,3 @@
 # -*- coding: utf-8 -*-
 #YYYY.MM.DD.DAY_RELEASE
-<<<<<<< HEAD
-IPTV_VERSION = "2022.01.10.01"
-=======
-IPTV_VERSION="2022.01.10.02"
->>>>>>> 81ed79d0... add parser evoload.io
+IPTV_VERSION = "2022.01.15.01"
